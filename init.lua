64178c64586a1cfeea0ddddfe4703bf041c4429d
rgt <rasbin@outlook.com>
2025-01-09 01:31:00 +0545

    netrw opening in same tab with tree view

--- init.lua
+++ init.lua
@@ -3,5 +3,8 @@ require("config.remap")
 require("config.which-key")
 require("config.lsp-config")
 require("config.bufferline")
+require("config.treesitter")
+require("config.setup")
+
 
 
---
+++ lua/config/setup.lua
@@ -0,0 +1,9 @@
+-- Open directories in the same tab
+vim.g.netrw_browse_split = 0
+
+-- Optional: Additional netrw settings
+vim.g.netrw_banner = 0         -- Disable the banner
+vim.g.netrw_liststyle = 3      -- Use tree-style listing
+vim.g.netrw_altv = 1          -- Open splits to the right
+vim.g.netrw_winsize = 25       -- Set window size to 25% of the screen
+
