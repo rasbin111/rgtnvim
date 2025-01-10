414d69a1a76f22e74633d85d40d6314fecb98a6b
rasbin <rasbin.thapa@learnsic.com>
2025-01-10 12:02:55 +0545

    added line, relative line number and tab size

--- lua/config/setup.lua
+++ lua/config/setup.lua
@@ -1,6 +1,3 @@
-
-vim.opt.termguicolors = true
-
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -8,3 +5,13 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
+-- Set tab size
+vim.opt.tabstop = 4         -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 4      -- Number of spaces for each indentation level
+vim.opt.expandtab = true    -- Convert tabs to spaces
+
+-- Enable line numbers
+vim.opt.number = true
+
+-- Enable relatve line numbers
+vim.opt.relativenumber = true
