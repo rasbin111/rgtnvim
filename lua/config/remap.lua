9d320397d36955cbb3df5020053c8262729eac46
rgt <rasbin@outlook.com>
2025-01-12 22:33:23 +0545

    toggleterm added, a terminal inside neovim

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -10,3 +10,14 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc="Telescope find file
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- toggleterm
+-- Horizontal terminal
+vim.keymap.set("n", "<Leader>th", ":ToggleTerm direction=horizontal<CR>", { noremap = true, silent = true })
+
+-- Vertical terminal
+vim.keymap.set("n", "<Leader>tv", ":ToggleTerm direction=vertical<CR>", { noremap = true, silent = true })
+
+-- Floating terminal
+vim.keymap.set("n", "<Leader>tf", ":ToggleTerm direction=float<CR>", { noremap = true, silent = true })
+
