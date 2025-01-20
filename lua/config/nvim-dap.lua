04231272c6c38476d38e63fcb15571ea79731ad5
rasbin <rasbin.thapa@learnsic.com>
2025-01-20 17:33:59 +0545

    require nvim-dap added

--- lua/config/nvim-dap.lua
+++ lua/config/nvim-dap.lua
@@ -1,3 +1,2 @@
 require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
--- If using the above, then `/path/to/venv/bin/python -m debugpy --version`
--- must work in the shell
+require("dapui").setup()
