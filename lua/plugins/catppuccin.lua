54f2c072b7dfb06e39c4c6bfac3fa815f20404b4
rgt <rasbin@outlook.com>
2025-02-15 22:59:57 +0545

    removed catpuccin as default theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,5 +1,5 @@
 return { "catppuccin/nvim", name = "catppuccin", priority = 1000, 
-	config = function()
-		vim.cmd.colorscheme "catppuccin"
-	end
+	-- config = function()
+	-- 	vim.cmd.colorscheme "catppuccin"
+	-- end
 }
