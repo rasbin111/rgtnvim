10dfd061bc33ca95ad37745f59f37cf9de193e19
rgt <rasbin@outlook.com>
2025-03-04 19:40:18 +0545

    changed background transparency for neovide

--- lua/config/neovide-config.lua
+++ lua/config/neovide-config.lua
@@ -1,8 +1,9 @@
 if vim.g.neovide then
     vim.cmd("colorscheme nordic")
     vim.o.guifont = "JetBrainsMono Nerd Font:h12"
-    vim.g.neovide_transparency = 0.9-- Set window transparency (0 = fully transparent, 1 = solid)
-    vim.g.transparency = 0.9 -- Set background transparency
+    vim.g.neovide_transparency = 0.91 -- Set window transparency (0 = fully transparent, 1 = solid)
+    vim.g.transparency = 0.91 -- Set background transparency
+
     -- vim.g.neovide_font_features = { "calt", "liga" }
     -- vim.g.neovide_cursor_vfx_mode = "railgun"  -- Other modes: torpedo, pixiedust, sonicboom
 end
