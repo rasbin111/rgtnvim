dff76b42bd37bf2af89b79905cbae1f3ca092d31
rgt <rasbin@outlook.com>
2025-03-04 23:55:27 +0545

    open neovide in full screen

--- lua/config/neovide-config.lua
+++ lua/config/neovide-config.lua
@@ -4,6 +4,8 @@ if vim.g.neovide then
     vim.g.neovide_transparency = 0.91 -- Set window transparency (0 = fully transparent, 1 = solid)
     vim.g.transparency = 0.91 -- Set background transparency
 
+    vim.g.neovide_fullscreen = true -- Start Neovide in fullscreen mode
+
     -- vim.g.neovide_font_features = { "calt", "liga" }
     -- vim.g.neovide_cursor_vfx_mode = "railgun"  -- Other modes: torpedo, pixiedust, sonicboom
 end
