c4283496509a6eeba431e04b95a94b1f1676f806
rasbin <rasbingodarthapa@gmail.com>
2025-05-14 17:42:18 +0545

    minor updates for autocompletion

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -1,8 +1,49 @@
--- lsp-config.lua
+-- lsp-config.lua for lsp-zero v2.x (early version)
 
--- Learn the keybindings, see :help lsp-zero-keybindings
--- Learn to configure LSP servers, see :help lsp-zero-api-showcase
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+-- Import lsp-zero
+local lsp_zero = require('lsp-zero')
 
-lsp.setup()
+-- Basic setup with recommended defaults
+lsp_zero.preset('recommended')
+
+-- Define on_attach function for LSP-specific keybindings
+lsp_zero.on_attach(function(client, bufnr)
+  -- Default keybindings provided by lsp-zero
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- Setup Mason if you're using it
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  -- Add the servers you want to install
+  ensure_installed = {
+    -- 'tsserver',
+    -- 'rust_analyzer',
+    -- Add more as needed
+  },
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
+
+-- Configure lua_ls manually for Neovim configuration files
+require('lspconfig').lua_ls.setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false
+      },
+      telemetry = {
+        enable = false,
+      },
+    }
+  }
+})
+
+-- If you need to set up additional LSP servers directly:
+-- require('lspconfig').tsserver.setup({})
+-- require('lspconfig').rust_analyzer.setup({})
