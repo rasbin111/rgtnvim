c1e36421e7542f2700a00f3e1fad81a191c00f63
rgt <rasbin@outlook.com>
2025-05-18 10:37:03 +0545

    autocomplete and autoformat added

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -1,49 +1,82 @@
--- lsp-config.lua for lsp-zero v2.x (early version)
+-- lsp-config.lua for lsp-zero v2.x (early version) -- Import lsp-zero 
+local lsp_zero = require('lsp-zero') 
 
--- Import lsp-zero
-local lsp_zero = require('lsp-zero')
-
--- Basic setup with recommended defaults
-lsp_zero.preset('recommended')
-
--- Define on_attach function for LSP-specific keybindings
-lsp_zero.on_attach(function(client, bufnr)
-  -- Default keybindings provided by lsp-zero
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-
--- Setup Mason if you're using it
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  -- Add the servers you want to install
-  ensure_installed = {
-    -- 'tsserver',
-    -- 'rust_analyzer',
-    -- Add more as needed
-  },
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = {'lua_ls', 'rust_analyzer'},
   handlers = {
-    lsp_zero.default_setup,
-  },
-})
-
--- Configure lua_ls manually for Neovim configuration files
-require('lspconfig').lua_ls.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false
-      },
-      telemetry = {
-        enable = false,
-      },
-    }
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
   }
 })
 
--- If you need to set up additional LSP servers directly:
--- require('lspconfig').tsserver.setup({})
--- require('lspconfig').rust_analyzer.setup({})
+-- Format on Save
+local buffer_autoformat = function(bufnr)
+    local group = 'lsp_autoformat'
+    vim.api.nvim_create_augroup(group, { clear = false })
+    vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+
+    vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = bufnr,
+        group = group,
+        desc = 'LSP format on save',
+        callback = function()
+            -- note: do not enable async formatting
+            vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+        end,
+    })
+end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(event)
+        local id = vim.tbl_get(event, 'data', 'client_id')
+        local client = id and vim.lsp.get_client_by_id(id)
+        if client == nil then
+            return
+        end
+
+        -- make sure there is at least one client with formatting capabilities
+        if client.supports_method('textDocument/formatting') then
+            buffer_autoformat(event.buf)
+        end
+    end
+})
+
+
+local cmp = require('cmp')
+
+cmp.setup({
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+    },
+    snippet = {
+        expand = function(args)
+          -- You need a snippet engine like luasnip or vsnip here
+          require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        -- Simple tab complete
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            local col = vim.fn.col('.') - 1
+
+            if cmp.visible() then
+                cmp.select_next_item({ behavior = 'select' })
+            elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                fallback()
+            else
+                cmp.complete()
+            end
+        end, { 'i', 's' }),
+
+        -- Go to previous item
+        ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+
+        -- Press enter to complete selection
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), 
+    }),
+})
