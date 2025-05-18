51d046d1ddb319e996742720939655c84d3780b3
rgt <rasbin@outlook.com>
2025-05-18 22:10:46 +0545

    updated navigation

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -1,53 +1,49 @@
--- lsp-config.lua for lsp-zero v2.x (early version) -- Import lsp-zero 
-local lsp_zero = require('lsp-zero') 
+-- Load lsp-zero
+local lsp_zero = require('lsp-zero')
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  -- Replace the language servers listed here
-  -- with the ones you want to install
-  ensure_installed = {'lua_ls', 'rust_analyzer'},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  }
-})
-
--- Format on Save
-local buffer_autoformat = function(bufnr)
-    local group = 'lsp_autoformat'
-    vim.api.nvim_create_augroup(group, { clear = false })
-    vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
-
-    vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = bufnr,
-        group = group,
-        desc = 'LSP format on save',
-        callback = function()
-            -- note: do not enable async formatting
-            vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-        end,
-    })
-end
-
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(event)
-        local id = vim.tbl_get(event, 'data', 'client_id')
-        local client = id and vim.lsp.get_client_by_id(id)
-        if client == nil then
-            return
-        end
-
-        -- make sure there is at least one client with formatting capabilities
-        if client.supports_method('textDocument/formatting') then
-            buffer_autoformat(event.buf)
-        end
+-- Configure lsp-zero
+lsp_zero.on_attach(function(client, bufnr)
+    -- Format on Save
+    if client.supports_method('textDocument/formatting') then
+        local group = 'lsp_autoformat'
+        vim.api.nvim_create_augroup(group, { clear = false })
+        vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = bufnr,
+            group = group,
+            callback = function()
+                vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+            end,
+        })
     end
+
+    -- LSP Keybindings
+    local opts = { buffer = bufnr, remap = false }
+    local keymap = vim.keymap.set
+
+    keymap("n", "gd", vim.lsp.buf.definition, opts)
+    keymap("n", "gD", vim.lsp.buf.declaration, opts)
+    keymap("n", "gi", vim.lsp.buf.implementation, opts)
+    keymap("n", "gr", vim.lsp.buf.references, opts)
+    keymap("n", "K", vim.lsp.buf.hover, opts)
+    keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+end)
+
+
+-- Mason setup
+require('mason-lspconfig').setup({
+    ensure_installed = { 'lua_ls', 'rust_analyzer' },
+    handlers = {
+        function(server_name)
+            require('lsp-zero').default_setup(server_name) -- ✅ THIS USES YOUR on_attach!
+        end,
+    }
 })
 
 
+-- CMP Setup (optional, but usually goes here too)
 local cmp = require('cmp')
-
 cmp.setup({
     sources = {
         { name = 'nvim_lsp' },
@@ -55,15 +51,12 @@ cmp.setup({
     },
     snippet = {
         expand = function(args)
-          -- You need a snippet engine like luasnip or vsnip here
-          require('luasnip').lsp_expand(args.body)
+            require('luasnip').lsp_expand(args.body)
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        -- Simple tab complete
         ['<Tab>'] = cmp.mapping(function(fallback)
             local col = vim.fn.col('.') - 1
-
             if cmp.visible() then
                 cmp.select_next_item({ behavior = 'select' })
             elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
@@ -72,11 +65,7 @@ cmp.setup({
                 cmp.complete()
             end
         end, { 'i', 's' }),
-
-        -- Go to previous item
         ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
-
-        -- Press enter to complete selection
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), 
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
     }),
 })
